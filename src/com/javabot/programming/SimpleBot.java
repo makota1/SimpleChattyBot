f78a3dcfa4da72466f73cd23366161b50fc743fa
Oleksandr Makota <makota.alex@icloud.com>
2020-09-07 17:59:16 +0400

    minor change

--- src/com/javabot/programming/SimpleBot.java
+++ src/com/javabot/programming/SimpleBot.java
@@ -17,7 +17,7 @@ public class SimpleBot {
     static void greet(String assistantName, String birthYear) {
         System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + birthYear + ".");
-        System.out.println("Please, remind me your name.");
+        System.out.println("Please, remind me your name!");
     }
 
     static void remindName() {
